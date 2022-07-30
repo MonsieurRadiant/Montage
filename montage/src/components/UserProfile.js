3f13043c8923d6a8ffe2c9439f9bc08cc42e85dc
AureKM <mk.aure.gui@gmail.com>
2022-07-30 03:44:30 -0700

    update

--- montage/src/components/UserProfile.js
+++ montage/src/components/UserProfile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import {getUserData} from "../actions";
+import "./style/userProfile.css";
 
 function UserProfile() {
   const dispatch = useDispatch();
@@ -14,16 +15,30 @@ function UserProfile() {
   }
 
   useEffect(() => {
-    getProfileData()
+    getProfileData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const profileData = useSelector(state => state.others.profile.data);
 
+  const movies = useSelector(state => state.movies.movies);
+  const likedMovies = useSelector(state => state.others.userPreference.favouriteMovies);
+
   return (
     <div className = "UserProfile">
-      <div>
-        <h4>{profileData.fullName}</h4>
+      <div className="basicInfo">
+        <h3>{profileData.fullName}</h3>
+        <p>{profileData.email}</p>
+      </div>
+      <hr />
+      <div className="likedList">
+        <h4>Liked Movies</h4>
+        {movies.filter(movie => likedMovies.includes(movie._id)).map(filteredMovie => (
+          <div>
+            <p>{filteredMovie.MovieTitle}</p>
+            <img className="post" src={filteredMovie.imageData} />
+          </div>
+        ))}
       </div>
     </div>
   )
